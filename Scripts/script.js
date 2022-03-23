71382d64bed709e5a9bc5a40336e14549db0dd59
dow-mp <derin.mitchell@gmail.com>
2022-03-23 15:06:59 -0400

    complete responsive design for home, admin, and about pages; add header/footer to volunteer, successes, search pages

---
+++ Scripts/script.js
@@ -0,0 +1,32 @@
+// temporarily change the color of the div border for main links on the home page when they are moused over
+const changeFlag1Color = () => {
+    document.querySelector(".flag1").style.borderColor = "#247BA0";
+}
+
+const flag1ColorReverse = () => {
+    document.querySelector(".flag1").style.borderColor = "#CBD4C2";
+};
+
+const changeFlag2Color = () => {
+    document.querySelector(".flag2").style.borderColor = "#247BA0"
+};
+
+const flag2ColorReverse = () => {
+    document.querySelector(".flag2").style.borderColor = "#CBD4C2";
+};
+
+const changeFlag3Color = () => {
+    document.querySelector(".flag3").style.borderColor = "#247BA0"
+};
+    
+const flag3ColorReverse = () => {
+    document.querySelector(".flag3").style.borderColor = "#CBD4C2";
+};
+
+const changeFlag4Color = () => {
+    document.querySelector(".flag4").style.borderColor = "#247BA0"
+};
+
+const flag4ColorReverse = () => {
+    document.querySelector(".flag4").style.borderColor = "#CBD4C2";
+};
