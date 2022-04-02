22240650e21d667766956a264e3a3853d5d0bbbd
dow-mp <derin.mitchell@gmail.com>
2022-04-01 22:59:14 -0400

    add success page, connect all internal and social links, start working on event listener for home page border color toggle

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,32 +1,46 @@
 // temporarily change the color of the div border for main links on the home page when they are moused over
-const changeFlag1Color = () => {
-    document.querySelector(".flag1").style.borderColor = "#247BA0";
-}
+// const changeFlag1Color = () => {
+//     document.querySelector(".flag1").style.borderColor = "#247BA0";
+// }
 
-const flag1ColorReverse = () => {
-    document.querySelector(".flag1").style.borderColor = "#CBD4C2";
-};
+// const flag1ColorReverse = () => {
+//     document.querySelector(".flag1").style.borderColor = "#CBD4C2";
+// };
 
-const changeFlag2Color = () => {
-    document.querySelector(".flag2").style.borderColor = "#247BA0"
-};
+// const changeFlag2Color = () => {
+//     document.querySelector(".flag2").style.borderColor = "#247BA0"
+// };
 
-const flag2ColorReverse = () => {
-    document.querySelector(".flag2").style.borderColor = "#CBD4C2";
-};
+// const flag2ColorReverse = () => {
+//     document.querySelector(".flag2").style.borderColor = "#CBD4C2";
+// };
 
-const changeFlag3Color = () => {
-    document.querySelector(".flag3").style.borderColor = "#247BA0"
-};
+// const changeFlag3Color = () => {
+//     document.querySelector(".flag3").style.borderColor = "#247BA0"
+// };
     
-const flag3ColorReverse = () => {
-    document.querySelector(".flag3").style.borderColor = "#CBD4C2";
+// const flag3ColorReverse = () => {
+//     document.querySelector(".flag3").style.borderColor = "#CBD4C2";
+// };
+
+// const changeFlag4Color = () => {
+//     document.querySelector(".flag4").style.borderColor = "#247BA0"
+// };
+
+// const flag4ColorReverse = () => {
+//     document.querySelector(".flag4").style.borderColor = "#CBD4C2";
+// };
+
+// onmouseenter="changeFlag2Color()" onmouseleave="flag2ColorReverse()"
+
+
+// WORK IN PROGRESS
+const flags = document.querySelectorAll("flag");
+const changeFlagColor = (e) => {
+    document.querySelectorAll("flag").forEach(flag=>{
+        flag.classList.toggle("flagClicked");
+    })
 };
 
-const changeFlag4Color = () => {
-    document.querySelector(".flag4").style.borderColor = "#247BA0"
-};
-
-const flag4ColorReverse = () => {
-    document.querySelector(".flag4").style.borderColor = "#CBD4C2";
-};
+flags.forEach(flag=>{flag.addEventListener("mouseenter", changeFlagColor)});
+flags.forEach(flag=>{flag.addEventListener("mouseleave", changeFlagColor)});
