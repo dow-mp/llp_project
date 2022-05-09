f52fb648756507fd84475f250106b946305443b7
dow-mp <derin.mitchell@gmail.com>
2022-05-09 15:12:50 -0400

    add ejs to pages to display information based on routers/controllers

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -9,24 +9,24 @@ router.route('/')
 router.route('/parks')
     .get(adminCtrl.park_all)
 
-router.route('/create-park')
+router.route('/add-park')
     .get(adminCtrl.create_park)
 
 router.route('/:_id/edit-park')
     .get(adminCtrl.park_update_get)
 
-// I don't think this belongs here...
-router.route('/:_id/delete-park')
-    .get(adminCtrl.park_delete)
-
 router.route('/volunteers')
     .get(adminCtrl.volunteer_all)
 
-router.route('/create-volunteer')
+router.route('/add-volunteer')
     .get(adminCtrl.create_volunteer)
 
 router.route('/:_id/edit-volunteer')
     .get(adminCtrl.volunteer_update_get)
+
+// I don't think this belongs here...
+router.route('/:_id/delete-park')
+    .get(adminCtrl.park_delete)
 
 // I don't think this belongs here...
 router.route('/:_id/delete-volunteer')
