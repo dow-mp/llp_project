7b16c69608c5fb669bcef90f2d09da492d332763
dow-mp <derin.mitchell@gmail.com>
2022-05-30 21:23:35 -0400

    update port for deployment

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride = require('method-override');
 const path = require('path');
 const app = express();
 const routes = require('./routes/indexRoutes')
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.set('view engine', 'ejs');
 
